abb014e4543a27d02f62a416faf861ff60c51434
Jan-Willem van der Sijp <gecko-git@parseq.co.uk>
2020-06-02 15:32:11 +0200

    Re-add plugins as non-submodules

---
+++ plugged/nvim-lsp/lua/nvim_lsp/pyls.lua
@@ -0,0 +1,24 @@
+local configs = require 'nvim_lsp/configs'
+local util = require 'nvim_lsp/util'
+
+configs.pyls = {
+  default_config = {
+    cmd = {"pyls"};
+    filetypes = {"python"};
+    root_dir = function(fname)
+      return util.path.dirname(fname)
+    end;
+  };
+  docs = {
+    package_json = "https://raw.githubusercontent.com/palantir/python-language-server/develop/vscode-client/package.json";
+    description = [[
+https://github.com/palantir/python-language-server
+
+`python-language-server`, a language server for Python.
+    ]];
+    default_config = {
+      root_dir = "vim's starting directory";
+    };
+  };
+};
+-- vim:et ts=2 sw=2
