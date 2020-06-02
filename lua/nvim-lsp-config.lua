62d668b3a4f7f49fcd04b7d155b1ba8e1f81f46b
Jan-Willem van der Sijp <gecko-git@parseq.co.uk>
2020-06-02 15:21:54 +0200

    Minimal reproducer for bug report

--- lua/nvim-lsp-config.lua
+++ lua/nvim-lsp-config.lua
@@ -2,74 +2,8 @@ local lsp = require'nvim_lsp'
 
 local M = {}
 
-local lsp_loaded_tbl = {}
-
-local function on_attach_vim()
-    require'diagnostic'.on_attach()
-
-    vim.api.nvim_command('nnoremap <silent> <buffer> ]d :NextDiagnostic<CR>')
-    vim.api.nvim_command('nnoremap <silent> <buffer> [d :PrevDiagnostic<CR>')
-
-    local capabilities = vim.lsp.buf_get_clients()[
-            next(vim.lsp.buf_get_clients())
-        ].resolved_capabilities
-    local function create_conditional_map(keys, funcname)
-        vim.api.nvim_command(
-            'nnoremap <silent> <buffer> '
-            .. keys
-            .. ' <cmd>lua '
-            .. 'vim.lsp.buf.'
-            .. funcname
-            .. '()<CR>'
-        )
-    end
-
-    if capabilities.declaration then
-        create_conditional_map('gd', 'declaration')
-    end
-    if capabilities.document_symbol then
-        create_conditional_map('g0', 'document_symbol')
-    end
-    if capabilities.hover then
-        create_conditional_map('K', 'hover')
-    end
-    if capabilities.implementation then
-        create_conditional_map('gD', 'implementation')
-    end
-    if capabilities.find_references then
-        create_conditional_map('gr', 'references')
-    end
-    if capabilities.signature_help then
-        create_conditional_map('<C-k>', 'signature_help')
-    end
-    if capabilities.type_definition then
-        create_conditional_map('1gD', 'type_definition')
-    end
-    if capabilities.workspace_symbol then
-        create_conditional_map('gW', 'workspace_symbol')
-    end
-
-    if capabilities.document_range_formatting then
-        vim.api.nvim_command(
-            'command! -range=% -buffer LspFormat '
-            .. 'lua vim.lsp.buf.range_formatting('
-            .. '{}, {<line1>, 1}, {<line2>, 1}'
-            .. ')'
-        )
-    elseif capabilities.document_formatting then
-        vim.api.nvim_command(
-            'command! -buffer LspFormat '
-            .. 'lua vim.lsp.buf.formatting()'
-        )
-    end
-end
-
 local function lsp_test_and_load(lserver, settings)
     local found = false
-
-    if lsp_loaded_tbl[lserver] == nil then
-        lsp_loaded_tbl[lserver] = false
-    end
 
     if lsp[lserver].install_info ~= nil
         and lsp[lserver].install_info().binaries ~= nil
@@ -96,10 +30,8 @@ local function lsp_test_and_load(lserver, settings)
         )
     then
         lsp[lserver].setup{
-            on_attach=on_attach_vim,
             settings = settings,
         }
-        lsp_loaded_tbl[lserver] = true
     end
 end
 
