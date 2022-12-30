f26fcb20a67d43759abcf4d481f5ed55e35e13c8
Skye van der Sijp <gecko-git@parseq.co.uk>
2022-12-30 16:19:29 +0100

    Add bufferline

---
+++ lua/my/plugins/bufferline.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    'akinsho/bufferline.nvim',
+    dependencies = {
+      { 'nvim-tree/nvim-web-devicons' },
+    },
+    config = {
+      options = {
+        view = 'multiwindow',
+        numbers = function(opts)
+          return string.format('%s ', opts.id)
+        end,
+        diagnostics = 'nvim_lsp',
+        custom_filter = function(bufnr)
+          return vim.bo[bufnr].filetype ~= 'qf'
+        end,
+        offsets = {
+          {
+            filetype = 'NvimTree',
+            text = 'Files',
+            text_align = 'left',
+          },
+        },
+      },
+    },
+  },
+}
